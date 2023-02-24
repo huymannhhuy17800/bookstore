3952862dd847492086de9aa42bf73882bdec4fc6
huymannhhuy17800 <huymanhhuy178@gmail.com>
2023-02-24 17:03:40 +0700

    first commit

--- src/main/java/com/huynm/bookstore/controller/LoginController.java
+++ src/main/java/com/huynm/bookstore/controller/LoginController.java
@@ -2,10 +2,13 @@ package com.huynm.bookstore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.huynm.bookstore.model.LoginDTO;
 import com.huynm.bookstore.repository.UserRepository;
 
 @Controller
@@ -14,10 +17,17 @@ public class LoginController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@PostMapping("checkLogin")
-	public String checkLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
-		userRepository.findByUsername(username);
-		if(userRepository..equals(username))
-		return "cart.html";
+	@RequestMapping(value="/login")
+	public String getLoginForm() {
+		return "login.html";
 	}
+	
+	@PostMapping(value="/login")
+	public String checkLogin(@ModelAttribute(name="loginDTO") LoginDTO loginDTO,Model model) {
+		String username = loginDTO.getUsernameOrEmail();
+		String password = loginDTO.getPassword();
+		
+		if()
+	}
+	
 }
